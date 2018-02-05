f209a4b1c86eaec7f169dfa4dcb560b7f572e651
p4v4n <pavan.barcelona@gmail.com>
2018-02-05 16:28:57 +0530

    Set encoding to utf-8

--- init.el
+++ init.el
@@ -54,3 +54,8 @@
 
 ;;stop making backup(~) files
 (setq make-backup-files nil)
+
+;; set encoding to utf-8
+(prefer-coding-system 'utf-8)
+(setq-default buffer-file-coding-system 'utf-8-auto-unix)
+
