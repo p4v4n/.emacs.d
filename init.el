7d81bd20ccb33d34511596c77b9db83518316ec7
p4v4n <pavan.barcelona@gmail.com>
2018-04-01 21:37:44 +0530

    Replace ido-mode with ivy+counsel+swiper

--- init.el
+++ init.el
@@ -84,10 +84,9 @@
 (setq apropos-sort-by-scores t)
 
 ;;ido-mode-interactively do things with buffers and files
-
-(setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
-(ido-mode 1)
+;; (setq ido-enable-flex-matching t)
+;; (setq ido-everywhere t)
+;; (ido-mode 1)
 
 ;;replace list-buffers with ibuffer(C-x C-b)
 (defalias 'list-buffers 'ibuffer)
@@ -132,6 +131,34 @@
 (use-package company
   :ensure t
   :config (global-company-mode))
+
+;;counsel: collection of Ivy-enhanced versions of common Emacs commands.
+(use-package counsel
+  :ensure t)
+
+;;ivy: a generic completion mechanism for Emacs
+;;swiper: an Ivy-enhanced alternative to isearch.
+(use-package swiper
+  :ensure try
+  :config (ivy-mode 1)
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (global-set-key "\C-s" 'swiper)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "<f6>") 'ivy-resume)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+  (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+  (global-set-key (kbd "<f1> l") 'counsel-find-library)
+  (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+  (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (global-set-key (kbd "C-x l") 'counsel-locate)
+  (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+  (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history))
 
 ;;use magit for git
 (use-package magit
