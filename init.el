47184346433f20f9d5a7cd40c1f8e428e953ea8b
p4v4n <pavan.barcelona@gmail.com>
2018-09-01 10:26:13 +0530

    Avoid garbage collection during startup to improve emacs-init-time.

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 ;; BASIC CUSTOMIZATION
 
+;; turn up the memory threshold to prevent garbage collection from running during startup
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 0.6)
+
 ;;-------packages-----------
 
 (require 'package)
@@ -31,23 +35,24 @@
 ;; Move custom-config to a seperate file
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
-;;----------------------------------
+
 ;;set my todo-list as default buffer on startup if it exists
 (if (file-exists-p "~/.emacs.d/todo-lists/daily-list.org")
     (setq initial-buffer-choice "~/.emacs.d/todo-lists/daily-list.org"))
-;;---------------
 
 ;;load all customizations from myinit.org
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-
-;;------------
 
 ;;set default-browser to chromium
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "chromium-browser")
 
-;;-----------
 ;; load user customizations
 (when (file-exists-p "~/.emacs.d/init-user.el")
   (setq user-custom-file "~/.emacs.d/init-user.el")
   (load user-custom-file))
+
+;;reset gc values to something reasonable after startup
+(add-hook 'emacs-startup-hook
+	  (lambda () (setq gc-cons-threshold 16777216
+		gc-cons-percentage 0.1)))
