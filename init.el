3c9c0f5d31dccc21385ae3741f78916713016147
p4v4n <pavan.barcelona@gmail.com>
2018-09-10 16:44:27 +0530

    Remove a duplicate entry of gnu repository from package-archives.
    - Hoping this would fix the issue with duplicate packages in the elpa folder

--- init.el
+++ init.el
@@ -11,7 +11,6 @@
 ;;add more repositories to fetch-archive list
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 
